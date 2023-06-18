163fbb9829e369e48b7489cb26f3b9fa622b79b5
nomiss7 <mrsterixmr@gmail.com>
2023-06-18 21:10:06 +0300

    7.7

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
-TOKEN = '6204831220:AAHyoUG9ozK1JGlPVDgMd5MqV_FpxJ7POrk'
+import os
+from dotenv import load_dotenv
 
-IP = 'localhost'
-PGUSER = 'postgres'
-PGPASSWORD = '7'
-DATABASE = 'database'
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+IP = os.getenv('IP')
+PGUSER = os.getenv('PGUSER')
+PGPASSWORD = os.getenv('PGPASSWORD')
+DATABASE = os.getenv('DATABASE')
